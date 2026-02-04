0604461f8f0b215a5a7c6a8c9cfc1829144cab37
Aleksey <alexholm222@yandex.ru>
2026-02-04 12:41:19 +0300

    fix/filters

--- src/redux/filters/companyFilterSlice.js
+++ src/redux/filters/companyFilterSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  selectedPartnerships: [],
+  selectedPartnerships: JSON.parse(localStorage.getItem('filterCompanys')) || [],
   companiesList: [],
   isLoadingCompanies: false,
 };
